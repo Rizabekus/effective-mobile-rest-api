03a02e674923af86dd28e0ad26feb0b52f5f961b
Rizabekus <rizabek.zhampeisov@gmail.com>
2024-01-23 18:45:49 +0600

    fixed pagination

--- internal/handlers/getpeople.go
+++ internal/handlers/getpeople.go
@@ -11,30 +11,40 @@ func (handler *Handlers) GetPeople(w http.ResponseWriter, r *http.Request) {
 
 	queryParams := r.URL.Query()
 	people, err := handler.Service.PersonService.FilteredSearch(queryParams)
+	fmt.Println(people)
 	if err != nil {
 		http.Error(w, "Failed to get people", http.StatusInternalServerError)
 		fmt.Println(err)
 		return
 	}
-	page, err := strconv.Atoi(r.URL.Query().Get("page"))
-	if err != nil || page < 1 {
-		http.Error(w, "Invalid 'page' parameter", http.StatusBadRequest)
-		return
+	pageQuery := r.URL.Query().Get("page")
+	var page int
+	if pageQuery == "" {
+		page = 1
+	} else {
+		page, err = strconv.Atoi(pageQuery)
+		if err != nil {
+			http.Error(w, "Invalid 'page' parameter", http.StatusBadRequest)
+			return
+		}
 	}
-	fmt.Println(people)
-	pageSize, err := strconv.Atoi(r.URL.Query().Get("pageSize"))
 
-	if err != nil || pageSize < 1 {
-		http.Error(w, "Invalid 'pageSize' parameter", http.StatusBadRequest)
-		return
+	pageSizeQuery := r.URL.Query().Get("pageSize")
+	var pageSize int
+	if pageSizeQuery == "" {
+		pageSize = 15
+	} else {
+		pageSize, err = strconv.Atoi(pageSizeQuery)
+		if err != nil {
+			http.Error(w, "Invalid 'pageSzie' parameter", http.StatusBadRequest)
+			return
+		}
 	}
+
 	if page >= 1 && pageSize >= 1 {
 		people = handler.Service.PersonService.Pagination(page, pageSize, people)
 	}
-	// if people == nil {
-	// 	http.Error(w, "Invalid 'page' parameter", http.StatusBadRequest)
-	// 	return
-	// }
+
 	jsonData, err := json.Marshal(people)
 	if err != nil {
 		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
